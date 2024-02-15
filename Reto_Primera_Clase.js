8e8c00a676135b2a85f6ea41603a1340c52f5d1e
Cerylion <fullthrottle.moran@gmail.com>
2024-02-14 23:00:56 -0600

    los 2 retos de la primera clase están completos

--- Reto_Primera_Clase.js
+++ Reto_Primera_Clase.js
@@ -1,12 +1,12 @@
 // reto 1: suma de pares
 // si se mete un 4 debe dar 6
 
-const num = 4
-
-const createArray = (num) => {
+const input = 4
+const num = input
+const createArray = (num) => {  
   let arr = []
   for (let i = 1; i <= num; i++) {
-      arr.push(i)
+    arr.push(i)
   }
   return arr
 }
@@ -14,17 +14,29 @@ const arr = createArray(num)
 const sumpares = () => {
   let respuesta = 0
   arr.forEach((elem) => {
-  if (arr[elem] <= num) {
-    if (arr[elem] % 2 == 0) {
-      respuesta += arr[elem]
+    if (arr[elem] <= num) {
+      if (arr[elem] % 2 == 0) {
+        respuesta += arr[elem]
+      }
+      elem += 1
     }
-    elem += 1
-  }
-})
-console.log(respuesta)
+  })
+  console.log(`The sum of the pair numbers from the input is: ${respuesta}`)
 }
 sumpares()
 
 // reto 2: contar vocales (a, e, i, o, u)
+const frase = 'lAs cOsAs bUeNaS eN lA vida_se-hacen esperar 500 años'
 
-const frase = 'las cosas buenas en la vida se hacen esperar'
+const countVowels = () => {
+  let vowelCounter = 0  
+  frase.split('').forEach((letter) => {
+    const trueChar = letter.toLowerCase()
+    const vowelBox = ['a', 'e', 'i', 'o', 'u']
+    if (vowelBox.includes(trueChar)) {
+      vowelCounter += 1
+    }
+  })
+  console.log(`The amount of Vowels in the frase is: ${vowelCounter}`)
+}
+countVowels()
