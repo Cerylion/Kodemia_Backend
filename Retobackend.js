ff61567c51ab5f7e5789e62fbe5c40635c37e244
Cerylion <fullthrottle.moran@gmail.com>
2024-02-14 20:14:15 -0600

    first commit - lagunas mentales

---
+++ Retobackend.js
@@ -0,0 +1,17 @@
+// reto 1: suma de pares
+// si se mete un 4 debe dar 6
+
+const num = 4
+const sumpares = () => {
+  let arr = []
+  let i = 1
+  if (i <= num) {
+    if (i % 2 == 0)
+      arr.push(i)
+  i += 1
+  console.log(arr)
+} console.log(arr)
+}
+sumpares()
+
+// reto 2: contar vocales (a, e, i, o, u)
