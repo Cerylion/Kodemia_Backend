c6e984e3a5cd116f56e41639e1f28c5b1165976c
Cerylion <fullthrottle.moran@gmail.com>
2024-02-14 21:13:10 -0600

    suma de pares lograda

--- Retobackend.js
+++ Retobackend.js
@@ -2,16 +2,29 @@
 // si se mete un 4 debe dar 6
 
 const num = 4
-const sumpares = () => {
+
+const createArray = (num) => {
   let arr = []
-  let i = 1
-  if (i <= num) {
-    if (i % 2 == 0)
+  for (let i = 1; i <= num; i++) {
       arr.push(i)
-  i += 1
-  console.log(arr)
-} console.log(arr)
+  }
+  return arr
+}
+const arr = createArray(num)
+const sumpares = () => {
+  let respuesta = 0
+  arr.forEach((elem) => {
+  if (arr[elem] <= num) {
+    if (arr[elem] % 2 == 0) {
+      respuesta += arr[elem]
+    }
+    elem += 1
+  }
+})
+console.log(respuesta)
 }
 sumpares()
 
 // reto 2: contar vocales (a, e, i, o, u)
+
+const frase = 'las cosas buenas en la vida se hacen esperar'
